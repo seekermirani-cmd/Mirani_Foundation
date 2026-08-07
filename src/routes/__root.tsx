ca2f93c67e329a0117ac01372410fbbc00fbd0c7
seekermirani-cmd <seekermirani@gmail.com>
2026-08-07 11:02:46 +0530

    added icon logo of mirani foundation

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -97,7 +97,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "D:\Mirani-Foundation\Mirani_Foundation\src\assets\mirani-logo.png", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
